a1c1c50923dd730c912cbddb82d1d30c6b287c44
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-08-25 10:27:09 +0800

    Struct created

---
+++ Cat Facts (API)/DogFact.swift
@@ -0,0 +1,24 @@
+//
+//  DogFact.swift
+//  Cat Facts (API)
+//
+//  Created by Daniel on 25/8/18.
+//  Copyright © 2018 Placeholder Interactive. All rights reserved.
+//
+
+import Foundation
+
+
+struct DogFact: Codable {
+    var text: String
+    
+    enum CodingKeys: String, CodingKey {
+        case text
+    }
+    
+    init(from decoder:Decoder) throws {
+        let valueContainer = try decoder.container(keyedBy: CodingKeys.self)
+        self.text = try valueContainer.decode(String.self, forKey: CodingKeys.text)
+    }
+}
+
